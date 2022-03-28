d0f87e4d3f14634952ccd529d8f480e338f748fa
jakub.matejka <jakub.matejka@VT1-14>
2022-03-28 12:43:32 +0200

    cau monster

--- aplic/Program.cs
+++ aplic/Program.cs
@@ -5,29 +5,64 @@ slovnik.Add("INT", 10);
 
 Console.WriteLine("Zadej jméno své postavy:");
 Console.ReadLine();
+int potreba = (30);
 
-Random rnd = new Random();
-int cas = rnd.Next(5, 20);
-if (cas => 5 && cas =< 10)
+while (true)
 {
-    Console.WriteLine("Tvoje síla:" + (slovnik["STR"] + 10));
-    Console.WriteLine("Tvoje dexterita:" + (slovnik["DEX"] + 10));
-    Console.WriteLine("Tvoje inteligence:" + (slovnik["INT"] + 10));
+    Console.WriteLine("Potřebuješ srát na " + potreba + "%");
+    Console.WriteLine("Chceš se vysrat nebo jít na quest? (V/Q)");
+    string odpoved = Console.ReadLine();
+    Random HVN = new Random();
+    int pica = HVN.Next(0, 100);
+    if (pica < potreba )
+    {
+        Console.WriteLine("Umrel jsi pico");
+        System.Environment.Exit(0);
+    }
+     
+
+
+    Random rnd = new Random();
+    int cas = rnd.Next(5, 20);
+    if (odpoved == "V")
+    {
+        Console.WriteLine("Vysral ses, potřbuješ srát na " + (potreba -= 10) + "%");
+    }
+    if (odpoved == "Q")
+    {
+        
+        Console.WriteLine("Vydal ses na quest, čekej " + cas + "s");
+        Thread.Sleep(cas * 1000);
+    }
+
+    if (cas >= 5 && cas <= 10)
+    {
+        Console.WriteLine("Nevysral ses, tvoje potreba: " + (potreba += 10) + "%");
+        Console.WriteLine("Tvoje síla:" + (slovnik["STR"] += 10));
+        Console.WriteLine("Tvoje dexterita:" + (slovnik["DEX"] += 10));
+        Console.WriteLine("Tvoje inteligence:" + (slovnik["INT"] += 10));
+    }
+
+     if (cas >= 11 && cas <= 15)
+    {
+        Console.WriteLine("Nevysral ses, tvoje potreba: " + (potreba += 15) + "%");
+        Console.WriteLine("Tvoje síla:" + (slovnik["STR"] += 15));
+        Console.WriteLine("Tvoje dexterita:" + (slovnik["DEX"] += 15));
+        Console.WriteLine("Tvoje inteligence:" + (slovnik["INT"] += 15));
+    }
+     if (cas >= 16 && cas >= 20)
+    {
+        Console.WriteLine("Nevysral ses, tvoje potreba: " + (potreba += 20) + "%");
+        Console.WriteLine("Tvoje síla:" + (slovnik["STR"] += 20));
+        Console.WriteLine("Tvoje dexterita:" + (slovnik["DEX"] += 20));
+        Console.WriteLine("Tvoje inteligence:" + (slovnik["INT"] += 20));
+    }
+
 }
 
 
-int potreba = (100);
-Console.WriteLine("Potřebuješ srát na " + potreba + "%");
-Console.WriteLine("Chceš se vysrat nebo jít na quest? (V/Q)");
-Console.ReadLine();
 
-string odpoved = Console.ReadLine();
-if (odpoved == "V")
-{
-        Console.WriteLine("Vysral ses, potřbuješ srát na " + (potreba - 10));
-}
-else if (odpoved == "Q")
-{
-    Console.WriteLine("Vydal ses na quest, čekej" + rnd );
-    
-}
+
+
+
+
